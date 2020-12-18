36af4ac24968d196b4d24842453ffb601272a58b
Lukáš Hejtmánek <hejtmy@gmail.com>
2020-12-18 01:15:46 +0100

    Adds drop order performance

--- R/vremt-analysis.R
+++ R/vremt-analysis.R
@@ -9,11 +9,47 @@ vremt_collection_performance <- function(obj, index){
   collected_items <- get_collected_items(phase_obj)
   res <- list()
   res$item_performance <- item_performance(task$items[[1]], collected_items)
+  # Time
+  # Number of drops
+  # Path?
   return(res)
 }
 
-vremt_placemnt_performance <- function(obj, index){
+vremt_placement_performance <- function(obj, index){
+  # Correct order
+  phase_obj <- get_recallPlacement_data(obj, index)
+  if(is.null(phase_obj)){
+    warning("There is not a recall at index ", index)
+    return(NULL)
+  }
+  phase_task_index <- get_phase_task_index(phase_obj)
+  task <- get_task_settings(phase_obj, phase_task_index)
 
+  df_actions <- get_actions_log(phase_obj)
+  # participants can pick the object back and then drop it elsewhere, need to only
+  # count the last drop
+  drop <- select_last_drops(df_actions)
+
+  res <- list()
+  # order
+  correct_order <- task$items[[1]]
+  dropped_order <- drop$item_name
+  res$drop_order <- sapply(correct_order, function(x){
+    which(x == correct_order) - which(x == dropped_order)
+  })
+  # location
+
+  # path? Thats weird, as path should not mean anything
+  return(res)
+}
+
+select_last_drops <- function(df_actions){
+  drop <- df_actions[df_actions$action == "dropped", ]
+  drop <- drop[order(drop$timestamp, decreasing = TRUE), ]
+  ## remove duplicated user_id
+  drop <- drop[!duplicated(drop[, c("action", "item_name")]), ]
+  drop <- drop[order(drop$timestamp, decreasing = FALSE), ]
+  return(drop)
 }
 
 item_performance <- function(wanted_items, collected_items) {
