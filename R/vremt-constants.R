20bab1004b2cea0c704b68df023e1215eb9ab73b
Lukáš Hejtmánek <hejtmy@gmail.com>
2020-12-19 02:07:14 +0100

    Adds image

--- R/vremt-constants.R
+++ R/vremt-constants.R
@@ -1,1 +1,6 @@
 AREA_BOUNDARIES <- list(x = c(0,250), y = c(0, 250))
+
+IMG_BACKGROUND_PATH <- system.file("extdata/img/islands-simple.png",
+                                   package = "brainvr.vremt")
+IMG_BOUNDARIES <- list(x = c(-47,303), y = c(-32, 268))
+# nolint end
