cdfaa625e81fe3c54b6f8a1cdf81665f7b4259bf
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-07-11 11:53:58 +0200

    small updates code and descriptions

--- R/vremt-constants.R
+++ R/vremt-constants.R
@@ -1,7 +1,8 @@
-AREA_BOUNDARIES <- list(x = c(0,250), y = c(0, 250))
+AREA_BOUNDARIES <- list(x = c(0, 250), y = c(0, 250))
 
 get_background_image <- function(){
   system.file("extdata/img/islands-simple.png", package = "brainvr.vremt")
 }
-IMG_BOUNDARIES <- list(x = c(-47,303), y = c(-32, 268))
+
+IMG_BOUNDARIES <- list(x = c(-47, 303), y = c(-32, 268))
 # nolint end
